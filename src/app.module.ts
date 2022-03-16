1fcdc3e60dea39a6b67bbd5e87b0de0cef1275f2
kalinichev.ivan <kalinichev.ivan@geek.band>
2022-03-16 19:55:29 +0200

    fix env

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,22 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductsModule } from "./products/product.module";
-
+import { ProductsModule } from './products/product.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ProductsModule, MongooseModule.forRoot(`mongodb+srv://root:root@cluster0.nzmja.mongodb.net/products?retryWrites=true&w=majority`)],
+  imports: [
+    ProductsModule,
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@cluster0.nzmja.mongodb.net/products?retryWrites=true&w=majority`,
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
+ConfigModule.forRoot({
+  envFilePath: '.config.env',
+});
